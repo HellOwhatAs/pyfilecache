b21ece43acf3b9bbed46cf605ffea7daaa66e885
HellOwhatAs <xjq701229@outlook.com>
2024-05-30 20:13:40 +0800

    v0.1.2

--- pyfilecache/__init__.py
+++ pyfilecache/__init__.py
@@ -1,82 +1,115 @@
 from io import TextIOWrapper
 from pathlib import Path
-from typing import Any, Hashable, Callable, Tuple, Dict
-from functools import _make_key
-import pickle, inspect, threading
+from typing import Any, Hashable, Callable, Dict
+from functools import _make_key, partial
+import pickle, inspect, threading, shutil
 
 CACHE_DIR = '__pyfilecache__'
-KEY2INDEX_LOCK = threading.Lock()
 
-def key2findex(key: Hashable, index_dir: Path) -> Tuple[int, bool]:
-    KEY2INDEX_LOCK.acquire()
-    with open(index_dir, "rb") as f:
-        index: Dict[Any, int] = pickle.load(f)
-    result = index.get(key)
-    flag = result is None
-    if flag:
-        with open(index_dir, "wb") as f:
-            index[key] = len(index)
-            pickle.dump(index, f)
-    KEY2INDEX_LOCK.release()
-    return index[key], not flag
-    
-def file_cache(
-        user_function = None, *,
-        reader: Callable[[TextIOWrapper], Any] = None,
-        writer: Callable[[Any, TextIOWrapper], None] = None,
-        typed: bool = False
+class _FileCached:
+    def __init__(
+            self,
+            user_function,
+            reader: Callable[[TextIOWrapper], Any],
+            writer: Callable[[Any, TextIOWrapper], None],
+            typed: bool = False
     ):
-    
-    if reader is None: reader = pickle.load
-    if writer is None: writer = pickle.dump
-        
-    def wrapper_of_wrapper(user_function):
+        self.lock = threading.Lock()
 
-        code_path = Path(inspect.getfile(user_function))
-        cache_dir = code_path.parent.joinpath(CACHE_DIR)
-        cache_dir.mkdir(exist_ok=True)
-        funcache_dir = cache_dir.joinpath(f'{code_path.name}...{user_function.__qualname__}')
-        funcache_dir.mkdir(exist_ok=True)
-        index_dir = funcache_dir.joinpath('index')
-        if not index_dir.exists():
-            with open(index_dir, 'wb') as f:
+        self.reader, self.writer = reader, writer
+        self.typed = typed
+        self.user_function = user_function
+
+        self._build()
+
+    def _build(self):
+        """
+        init the index of CACHE_DIR
+        """
+        self.code_path = Path(inspect.getfile(self.user_function))
+        self.cache_dir = self.code_path.parent.joinpath(CACHE_DIR)
+        self.cache_dir.mkdir(exist_ok=True)
+        self.funcache_dir = self.cache_dir.joinpath(f'{self.code_path.name}...{self.user_function.__qualname__}')
+        self.funcache_dir.mkdir(exist_ok=True)
+        self.index_dir = self.funcache_dir.joinpath('index')
+        if not self.index_dir.exists():
+            with open(self.index_dir, 'wb') as f:
                 pickle.dump({}, f)
 
-        def wrapper(*args, **kwds):
-            key = tuple(_make_key((user_function.__code__.co_code, *args), kwds, typed))
-            idx, flag = key2findex(key, index_dir)
-            fidxp = funcache_dir.joinpath(f'_{idx}')
-            if flag and fidxp.exists():
-                with open(fidxp, 'rb') as f:
-                    return reader(f)
-            else:
-                result = user_function(*args, **kwds)
-                with open(fidxp, 'wb') as f:
-                    writer(result, f)
-                return result
-        return wrapper
+    def __call__(self, *args, **kwds):
+        """
+        wrapper of user_function
+        """
+        fidxp = self.fp(*args, **kwds)
+        if fidxp.exists():
+            with open(fidxp, 'rb') as f:
+                return self.reader(f)
+        else:
+            result = self.user_function(*args, **kwds)
+            with open(fidxp, 'wb') as f:
+                self.writer(result, f)
+            return result
+        
+    def fp(self, *args, **kwds):
+        """
+        the file path where the result of user_function(*args, **kwds) stored
+        """
+        key = tuple(_make_key((self.user_function.__code__.co_code, *args), kwds, self.typed))
+        idx = self._key2findex(key)
+        return self.funcache_dir.joinpath(f'_{idx}')
+
+    def _key2findex(self, key: Hashable) -> int:
+        self.lock.acquire()
+        try:
+            with open(self.index_dir, "rb") as f:
+                index: Dict[Any, int] = pickle.load(f)
+            result = index.get(key)
+            if result is None:
+                with open(self.index_dir, "wb") as f:
+                    index[key] = len(index)
+                    pickle.dump(index, f)
+        finally:
+            self.lock.release()
+        return index[key]
     
-    if user_function is None: return wrapper_of_wrapper
-    return wrapper_of_wrapper(user_function)
-        
+    def clear(self):
+        """
+        clear all stored results of this function
+        """
+        shutil.rmtree(self.funcache_dir)
+        self._build()
 
-        
+def file_cache(
+    user_function = None, *,
+    reader: Callable[[TextIOWrapper], Any] = pickle.load,
+    writer: Callable[[Any, TextIOWrapper], None] = pickle.dump,
+    typed: bool = False
+):
+    """
+    decorator of a slow function that stores the result to disk  
 
-
-
-if __name__ == '__main__':
+    Usage Example:
+    ```py
     import pandas as pd
     from functools import partial
-    df_cache = partial(file_cache, reader=pd.read_csv, writer=pd.DataFrame.to_csv)
-    
-    @df_cache
+    from pyfilecache import file_cache
+
+    @file_cache(
+        reader=partial(pd.read_csv, index_col=0),
+        writer=pd.DataFrame.to_csv
+    )
     def func(a, b):
-        print(a, b)
+        print("func called with args", a, b)
         return pd.DataFrame(
             data = {
                 'col1': [1, 2],
                 'col2': [a, b]
             }
         )
-    
+
     print(func(3, 4))
+    print(func(3, 4))
+    ```
+    """
+    if user_function is None: return partial(_FileCached, reader=reader, writer=writer, typed=typed)
+    return _FileCached(user_function, reader, writer, typed)
